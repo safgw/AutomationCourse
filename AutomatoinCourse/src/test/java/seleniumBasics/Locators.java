a52bd335bf4caa51cebae29cba795e54faad83b8
Hima Ann <himaahere@gmail.com>
2025-08-08 14:08:07 +0400

    Updated with WebElement Commands

--- AutomatoinCourse/src/test/java/seleniumBasics/Locators.java
+++ AutomatoinCourse/src/test/java/seleniumBasics/Locators.java
@@ -26,6 +26,12 @@ public class Locators extends Base {
 		driver.findElement(By.xpath("//button[starts-with(text(),'Show ')]"));
 		driver.findElement(By.xpath("//button[@id='button-one' and @type='button']"));
 		driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));
+		driver.findElement(By.xpath("//div[contains (text(), 'Single Input Field')]//parent::div[@class='card']"));
+		driver.findElement(By.xpath("//div[@class='card']//child::button[@id='button-one']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//following::div[@class='card']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//preceding:: div[@class='card']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//ancestor::div"));
+		driver.findElement(By.xpath("//div[@class='card']//descendant::div"));
 		
 		
 	}
---
+++ AutomatoinCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -0,0 +1,44 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WebElementCommands extends Base {
+
+	public static void main(String[] args) {
+		
+		WebElementCommands webElmnt = new WebElementCommands();
+		webElmnt.initializeBrowser();
+		webElmnt.handlingElements();
+
+	}
+	
+	public void handlingElements()
+	{
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		
+		//sendKeys()
+		WebElement msgBox = driver.findElement(By.xpath("//input[@id='single-input-field']"));
+		msgBox.sendKeys("Input Form");
+		
+		//click()
+		WebElement bttn = driver.findElement(By.xpath("//button[@id='button-one']"));
+		bttn.click();
+		
+		//getText()
+		WebElement msg = driver.findElement(By.xpath("//div[@id='message-one']"));
+		System.out.println(msg.getText()); // o/p - Your Message : Input Form
+		
+		//clear()
+		msgBox.clear();
+		
+		//getCSSValue()
+		System.out.println(bttn.getCssValue("background-color")); // o/p = rgba(0, 105, 217, 1)
+		
+		//getTagName()
+		WebElement value = driver.findElement(By.id("value-a"));
+		System.out.println(value.getTagName()); // o/p - input
+		
+	}
+
+}
